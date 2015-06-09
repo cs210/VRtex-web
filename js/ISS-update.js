b63e7ae109571df4dbcee870f5ccefd1bfe1aa04
Xiaonan Tong <xiaonan@stanford.edu>
2015-06-08 22:12:24 -0700

    code cleanup (indentation)

--- js/ISS-update.js
+++ js/ISS-update.js
@@ -1,67 +1,67 @@
 $(function(){
-  var container;
+   var container;
 
-  var camera, scene, renderer, controls;
+   var camera, scene, renderer, controls;
 
-  var mouseX = 0, mouseY = 0;
+   var mouseX = 0, mouseY = 0;
 
-  var cx = -2500, cy = 2400;
-  init();
-  animate();
+   var cx = -2500, cy = 2400;
+   init();
+   animate();
 
-  function init() {
+   function init() {
 
-    container = document.getElementById( 'iss-container' );
+      container = document.getElementById( 'iss-container' );
 
-    camera = new THREE.PerspectiveCamera( 45, $(container).parent().width() / 550, 1, 16000 );
-    camera.position.z = 100;
+      camera = new THREE.PerspectiveCamera( 45, $(container).parent().width() / 550, 1, 16000 );
+      camera.position.z = 100;
 
-    // scene
-    scene = new THREE.Scene();
+      // scene
+      scene = new THREE.Scene();
 
-    var ambient = new THREE.AmbientLight( 0x404040 );
-    scene.add( ambient );
-    var hx = new THREE.HemisphereLight(0xffffff, 0x444444, 0.8)
-    scene.add(hx);
-    scene.fog = new THREE.Fog(0x274297, 1200, 14000);
+      var ambient = new THREE.AmbientLight( 0x404040 );
+      scene.add( ambient );
+      var hx = new THREE.HemisphereLight(0xffffff, 0x444444, 0.8)
+      scene.add(hx);
+      scene.fog = new THREE.Fog(0x274297, 1200, 14000);
 
-    var directionalLight = new THREE.DirectionalLight( 0xffeedd );
-    directionalLight.position.set( 0, 0, 1 );
-    scene.add( directionalLight );
+      var directionalLight = new THREE.DirectionalLight( 0xffeedd );
+      directionalLight.position.set( 0, 0, 1 );
+      scene.add( directionalLight );
 
 
-    // load ascii model
-    var jsonLoader = new THREE.JSONLoader();
-    jsonLoader.load( "ISS_JS.js", createScene );
-    // load binary model
-    console.log("Loading Mesh");
-    function createScene( geometry, materials ) {
-      console.log("LOADED!");
+      // load ascii model
+      var jsonLoader = new THREE.JSONLoader();
+      jsonLoader.load( "ISS_JS.js", createScene );
+      // load binary model
+      console.log("Loading Mesh");
+      function createScene( geometry, materials ) {
+         console.log("LOADED!");
 
-      $("#helix").hide("slow" ,function(){ $("#helix").remove(); } );
+         $("#helix").hide("slow" ,function(){ $("#helix").remove(); } );
 
-      var material = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: true});
-      material.opacity = 0.2;
-      material.transparent = true;
+         var material = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: true});
+         material.opacity = 0.2;
+         material.transparent = true;
 
-//      var mesh = new THREE.Mesh( geometry, new THREE.MeshFaceMaterial( materials ));
-      var mesh = new THREE.Mesh( geometry, material);
-      scene.add(mesh);
-      jsonLoader.load("js/shuttle.js", function(geom, mat){
-         var shuttleMat = new THREE.MeshBasicMaterial({color: 0xee0000});
-         shuttleMat.opacity = 0.7;
-         shuttleMat.transparent = true;
-         window.player = new THREE.Mesh(geom, shuttleMat );
-         window.player.scale.x = player.scale.y = 0.2;
-         window.player.scale.z = 0.2
+         //      var mesh = new THREE.Mesh( geometry, new THREE.MeshFaceMaterial( materials ));
+         var mesh = new THREE.Mesh( geometry, material);
+         scene.add(mesh);
+         jsonLoader.load("js/shuttle.js", function(geom, mat){
+            var shuttleMat = new THREE.MeshBasicMaterial({color: 0xee0000});
+            shuttleMat.opacity = 0.7;
+            shuttleMat.transparent = true;
+            window.player = new THREE.Mesh(geom, shuttleMat );
+            window.player.scale.x = player.scale.y = 0.2;
+            window.player.scale.z = 0.2
 
-         window.player.rotateY(-Math.PI/2);
+            window.player.rotateY(-Math.PI/2);
 
-         scene.add(window.player);
-         window.player.position = geometry.center();
-         render();
-      });
-    }
+            scene.add(window.player);
+            window.player.position = geometry.center();
+            render();
+         });
+      }
 
       window.addAstronaut = function(PX, PY, PZ){
          var astro = new THREE.Mesh(new THREE.SphereGeometry(80, 4,5), new THREE.MeshBasicMaterial({color: 0xeeee00}));
@@ -73,44 +73,44 @@ $(function(){
       }
 
 
-    renderer = new THREE.WebGLRenderer({alpha: true, antialias: false, precision: "lowp"});
-    renderer.setClearColor( 0xffffff, 0);
+      renderer = new THREE.WebGLRenderer({alpha: true, antialias: false, precision: "lowp"});
+      renderer.setClearColor( 0xffffff, 0);
 
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( $(container).parent().width(), 550 );
-    container.appendChild( renderer.domElement)
+      renderer.setPixelRatio( window.devicePixelRatio );
+      renderer.setSize( $(container).parent().width(), 550 );
+      container.appendChild( renderer.domElement)
 
-    camera.position.x = cx;
-    camera.position.y = cy;
-    //
-    window.addEventListener( 'resize', onWindowResize, false );
+      camera.position.x = cx;
+      camera.position.y = cy;
+      //
+      window.addEventListener( 'resize', onWindowResize, false );
 
-  }
+   }
 
-  function onWindowResize() {
-    camera.aspect = $(container).parent().width() / 550;
-    camera.updateProjectionMatrix();
-    //        renderer.setSize( window.innerWidth, window.innerHeight );
+   function onWindowResize() {
+      camera.aspect = $(container).parent().width() / 550;
+      camera.updateProjectionMatrix();
+      //        renderer.setSize( window.innerWidth, window.innerHeight );
 
-  }
+   }
 
 
-  //
+   //
 
-  function animate() {
-    requestAnimationFrame( animate );
-    //controls.update();
-    render();
-  }
+   function animate() {
+      requestAnimationFrame( animate );
+      //controls.update();
+      render();
+   }
 
-  var radius = 3600;
-  var t = 0;
-  function render() {
-    camera.position.x = scene.position.x + radius * Math.cos(t);
-    camera.position.z = scene.position.z + radius * Math.sin(t);
-    t += 0.004;
-    camera.lookAt( scene.position );
-    renderer.render( scene, camera );
-  }
+   var radius = 3600;
+   var t = 0;
+   function render() {
+      camera.position.x = scene.position.x + radius * Math.cos(t);
+      camera.position.z = scene.position.z + radius * Math.sin(t);
+      t += 0.004;
+      camera.lookAt( scene.position );
+      renderer.render( scene, camera );
+   }
 
 });
--- js/main.js
+++ js/main.js
@@ -1,114 +1,115 @@
 socket = io.connect('http://127.0.0.1', { port: 8082, rememberTransport: false});
 socket.on('connect', function() {
-    // sends to socket.io server the host/port of oscServer and oscClient
-    socket.emit('config',
-        {
-            // where should we listen for messages for browser consumption?
-            server: {
-                port: 3334,
-                host: '127.0.0.1'
-            },
-            // where should we send messages from the browser?
-            client: {
-                port: 4000,
-                host: '127.0.0.1'
-            }
-        }
-    );
+   // sends to socket.io server the host/port of oscServer and oscClient
+   socket.emit('config',
+               {
+      // where should we listen for messages for browser consumption?
+      server: {
+         port: 3334,
+         host: '127.0.0.1'
+      },
+      // where should we send messages from the browser?
+      client: {
+         port: 4000,
+         host: '127.0.0.1'
+      }
+   }
+              );
 });
 
 window.astros = {};
 
 socket.on('message', function(message) {
-  var addr = message[0].substr(4);
-  var args = message.splice(1);
-  switch (addr) {
-  case "/position":
-    updatePosition.apply(this, args)
-    break;
-  case "/direction":
-    updateDirection.apply(this, args)
-    break;
-  case "/thrust":
-    updateThrust.apply(this, args)
-    break;
-  case "/log":
-    console.log("Log:", args);
-    break;
-  case "/update":
-    updateDirection.apply(this, args.slice(0, 3))
-    updateThrust.apply(this, args.slice(3, 4))
-    updatePosition.apply(this, args.slice(4, 7))
-    break;
-case "/health":
-    console.log(args);
-case "/astro":
+   var addr = message[0].substr(4);
+   var args = message.splice(1);
+   switch (addr) {
+      case "/position":
+         updatePosition.apply(this, args)
+         break;
+      case "/direction":
+         updateDirection.apply(this, args)
+         break;
+      case "/thrust":
+         updateThrust.apply(this, args)
+         break;
+      case "/log":
+         console.log("Log:", args);
+         break;
+      case "/update":
+         updateDirection.apply(this, args.slice(0, 3))
+         updateThrust.apply(this, args.slice(3, 4))
+         updatePosition.apply(this, args.slice(4, 7))
+         break;
+      case "/health":
+         console.log(args);
+      case "/astro":
 
-      var id = args[0];
-      var x = (args[1]  - 3500)/3;
-      var y = (args[2] + 110)/3;
+         var id = args[0];
+         var x = (args[1]  - 3500)/3 - 40;
+         var y = (args[3] + 110)/3 - 290;
+         var z = (args[2] - 430)/3 + 180;
 
-    break;
-  default:
-    console.error("Invalid OSC address", message);
-    break;
-  }
+         break;
+      default:
+         console.error("Invalid OSC address", message);
+         break;
+   }
 });
 
 var updatePosition = function (a, b, c) {
-  window.player.position.x = (a - 3550)/3 - 40;
-  window.player.position.y = (c + 110)/3 - 290;
-  window.player.position.z = (b - 430)/3 + 180;
-//  return;
-//  console.log("updatePosition", x, y, z);
-//  var newq = [z - 1000, x/3.5];
-//  var oldq = $('#position-indicator').offset();
-//  $('#position-indicator').css({ top: newq[0], left: newq[1] });
+   window.player.position.x = (a - 3550)/3 - 40;
+   window.player.position.y = (c + 110)/3 - 290;
+   window.player.position.z = (b - 430)/3 + 180;
+   //  return;
+   //  console.log("updatePosition", x, y, z);
+   //  var newq = [z - 1000, x/3.5];
+   //  var oldq = $('#position-indicator').offset();
+   //  $('#position-indicator').css({ top: newq[0], left: newq[1] });
 };
 
 var INDICATOR_DIAMETER = $("#direction-indicator").height();
 var updateDirection = function (yaw, pitch, roll) {
-  console.log(pitch, yaw, roll);
-  window.sphere.rotation.x = yaw/180 * Math.PI;
-  window.sphere.rotation.z = -1 * roll/180 * Math.PI;
-  window.sphere.rotation.y = -1 * pitch/180 *Math.PI;
+   console.log(pitch, yaw, roll);
+   window.sphere.rotation.x = yaw/180 * Math.PI;
+   window.sphere.rotation.z = -1 * roll/180 * Math.PI;
+   window.sphere.rotation.y = -1 * pitch/180 *Math.PI;
 
    var c = window.sphere.rotation;
-  window.player.rotation.set(c.x, c.y - Math.PI / 2, c.z);
+   window.player.rotation.set(c.x, c.y - Math.PI / 2, c.z);
 
-//  var scale = 125;
-//  var newX, newY;
-//  console.log("updateDirection", pitch, yaw, roll);
-//  pitch = pitch / 22;
-//  yaw = yaw / 22;
-//
-//  if (pitch > 0 && false) {
-//    newY = (1-pitch) * scale;
-//  } else {
-//    newY = scale + (-1 * pitch * scale);
-//  }
-//
-//  if (yaw > 0 && false) {
-//    newX = (1-yaw) * scale;
-//  } else {
-//    newX = scale + (yaw * scale);
-//  }
-//
-//  // Subtract half the width/height of the indicator
-//  var newq = [newY + INDICATOR_DIAMETER, newX - INDICATOR_DIAMETER / 2];
-//  var oldq = $('#direction-indicator').offset();
-//  $('#direction-indicator').css({ top: newq[0], left: newq[1] });
+   //  var scale = 125;
+   //  var newX, newY;
+   //  console.log("updateDirection", pitch, yaw, roll);
+   //  pitch = pitch / 22;
+   //  yaw = yaw / 22;
+   //
+   //  if (pitch > 0 && false) {
+   //    newY = (1-pitch) * scale;
+   //  } else {
+   //    newY = scale + (-1 * pitch * scale);
+   //  }
+   //
+   //  if (yaw > 0 && false) {
+   //    newX = (1-yaw) * scale;
+   //  } else {
+   //    newX = scale + (yaw * scale);
+   //  }
+   //
+   //  // Subtract half the width/height of the indicator
+   //  var newq = [newY + INDICATOR_DIAMETER, newX - INDICATOR_DIAMETER / 2];
+   //  var oldq = $('#direction-indicator').offset();
+   //  $('#direction-indicator').css({ top: newq[0], left: newq[1] });
 };
 
 var updateThrust = function (percent) {
-  percent = percent / 8;
-  console.log("updateThrust", percent);
-  $("#thrust-meter").width(percent + 50 + "%");
-  if(Math.abs(percent) < 5){
-    $("#thrust-meter").css("background-color", "white");
-  }else if(percent > 0){
-    $("#thrust-meter").css("background-color", "aquamarine");
-  }else{
-    $("#thrust-meter").css("background-color", "red");
-  }
+   percent = percent / 8;
+   console.log("updateThrust", percent);
+   $("#thrust-meter").width(percent + 50 + "%");
+   if(Math.abs(percent) < 5){
+      $("#thrust-meter").css("background-color", "white");
+   }else if(percent > 0){
+      $("#thrust-meter").css("background-color", "aquamarine");
+   }else{
+      $("#thrust-meter").css("background-color", "red");
+   }
 };
